6aecc39442c2c3f6fcef49fbbf86c042dc0b2c8a
Ian Boyle <irboyle21@gmail.com>
2021-12-09 11:18:34 -0500

    adds getSectors method to industriesShow - percentage of sector in industry now shown

--- src/views/industries/IndustriesShow.vue
+++ src/views/industries/IndustriesShow.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="industries-show">
     Industry: {{ industry.industry }} | Percent of Account: {{ industry.industry_percent_of_account }}% |
-    {{ industry.sectors }} Sectors:
-    <div v-for="sector in sectors" :key="sector.id">{{ sector.sector }}</div>
+    {{ industry.sector_percent_of_industry }} | Sectors:
 
+    <div v-for="sector in sectors" :key="sector.id">
+      <h4>{{ sector.sector }}:</h4>
+      <p>
+        Percent of Industry:
+        {{ (sector.sector_percent_of_account / industry.industry_percent_of_account).toFixed(3) * 100 }} %
+      </p>
+    </div>
     <div>
       <router-link v-bind:to="`/industries/${industry.id}/edit`">
         <button type="button">Edit</button>
@@ -37,8 +43,8 @@ export default {
   created: function () {
     axios.get("http://localhost:3000/industries/" + this.$route.params.id).then((response) => {
       this.industry = response.data;
-      this.sectors = response.data.sectors;
-      console.log("Success", response.data, this.sectors);
+      console.log("Success", response.data);
+      this.getSectors();
     });
   },
   methods: {
@@ -46,6 +52,15 @@ export default {
       axios.delete("http://localhost:3000/industries/" + this.$route.params.id).then((response) => {
         console.log(response.data);
         this.$router.push("/industries");
+      });
+    },
+    // gets sectors pertaining to this industry to use sector info from backend
+    getSectors: function () {
+      this.industry.sectors.forEach((sector) => {
+        axios.get("http://localhost:3000/sectors/" + sector.id).then((response) => {
+          this.sectors.push(response.data);
+        });
+        console.log("Sectors", this.sectors);
       });
     },
   },
