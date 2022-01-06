6d25433d0adbbe7725a53249729e150c01afde24
Ian Boyle <irboyle21@gmail.com>
2022-01-06 11:04:08 -0500

    adds axios default url to main.js file

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "https://fierce-taiga-06308.herokuapp.com" : "/";
+Vue.config.productionTip = false;
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
   axios.defaults.headers.common["Authorization"] = "Bearer " + jwt;
